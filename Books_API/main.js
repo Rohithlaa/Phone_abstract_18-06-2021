a8d343150fb7ac258e4421e0757168b27fc454e5
Rohith <k.rohithkumar27@gmail.com>
2021-06-17 18:50:00 +0530

    Last Change

---
+++ Books_API/main.js
@@ -0,0 +1,20 @@
+var API_Key = "RMH04Gt3UGfpMAQ10FIyBNlWo4UEv5mk"
+const url = `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${API_Key}`
+async function funcName(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    var temp=""
+    for(var i=0;i<data.results.length;i++){
+        document.getElementById('data').innerHTML = " "
+        temp += "<tr>"
+        temp+= "<td>" + i + "</td>"
+        temp+= "<td>" + data.results[i].display_name+"</td>"
+        temp+= "<td>" + data.results[i].list_name+"</td>"
+        temp+= "<td>" + data.results[i].newest_published_date+"</td>"
+        temp+= "<td>" + data.results[i].oldest_published_date+"</td>"
+        temp+= "<td>"+data.results[i].updated+"</td></tr>"
+    }
+    document.getElementById('data').innerHTML = temp
+    document.getElementById('foot').innerHTML="<p>"+data.copyright+"</p>"
+}
+funcName(url)
